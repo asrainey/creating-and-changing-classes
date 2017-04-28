23aa7838c3166c815968b79546ecd7229f785df3
Allison Rainey <allisonsrainey@gmail.com>
2017-04-28 19:41:16 -0400

    Completed factorial method

---
+++ factorial.rb
@@ -0,0 +1,15 @@
+class Integer
+  def factorial
+    if self < 0
+      return 'You can\'t take the factorial of a negative number'
+    end
+
+    if self <= 1
+      1
+    else
+      self * (self - 1).factorial
+    end
+  end
+end
+
+puts 10.factorial
